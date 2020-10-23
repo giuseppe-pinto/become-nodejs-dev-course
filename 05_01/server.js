ae1bbfeeb1e3ca71247a05fe667d634bdee119b0
gpinto <giuseppe.pinto@lastminute.com>
2020-10-23 18:05:49 +0200

    http request to the BE

--- 05_01/server.js
+++ 05_01/server.js
@@ -5,6 +5,17 @@ var app = express()
 //it takes the static file from the direcory
 app.use(express.static(__dirname))
 
+var messages = [
+
+    {name: 'Tim', message:'Hi'},
+    {name: 'Jane', message:'Hello'}
+
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
 var server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
