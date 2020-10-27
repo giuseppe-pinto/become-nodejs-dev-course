bdfa0da072226006670ab51139fcb9139b913373
gpinto <giuseppe.pinto@lastminute.com>
2020-10-27 21:46:58 +0100

    added example with event emitter

---
+++ node-essential-training/event-emitter/events.js
@@ -0,0 +1,22 @@
+const events = require("events")
+
+const emitter = new events.EventEmitter()
+
+
+emitter.on("customEvent", (message, user) => {
+    console.log(`${user}: ${message}`)
+})
+
+
+
+emitter.emit("customEvent", "Hello word", "Computer")
+emitter.emit("customEvent", "That's pretty cool huh?", "Alex")
+
+
+
+
+
+
+
+
+
